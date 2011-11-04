996336ed8e466858e3b7efc646e1d42037d1213d
Konstantin Pribluda <konstantin.pribluda@gmail.com>
2011-11-04 20:39:19 +0100

    initial structure creation continued

---
+++ src/de/pribluda/android/camerawatch/CameraWatch.java
@@ -0,0 +1,18 @@
+package de.pribluda.android.camerawatch;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: ko5tik
+ * Date: Nov 4, 2011
+ * Time: 8:35:38 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class CameraWatch extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
