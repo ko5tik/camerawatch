459742afce91e25faed53d52b33239d7065c4d1d
Konstantin Pribluda <konstantin.pribluda@gmail.com>
2011-12-11 15:42:29 +0100

    working on main activity

--- src/de/pribluda/android/camerawatch/CameraWatch.java
+++ src/de/pribluda/android/camerawatch/CameraWatch.java
@@ -2,6 +2,7 @@ package de.pribluda.android.camerawatch;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
 
 /**
  * main activity
@@ -10,5 +11,9 @@ public class CameraWatch extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.main);
+
     }
 }
