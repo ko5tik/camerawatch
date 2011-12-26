ff35816f31b46592d52efe0164e2f7d45e91ab2d
Konstantin Pribluda <konstantin.pribluda@gmail.com>
2011-12-26 22:17:30 +0100

    started work on settings

---
+++ src/de/pribluda/android/camerawatch/Settings.java
@@ -0,0 +1,17 @@
+package de.pribluda.android.camerawatch;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * configure settings
+ *
+ * @author Konstantin Pribluda
+ */
+public class Settings extends Activity {
+
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
